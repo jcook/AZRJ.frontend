26b70879266abfafb4eff6b2ae11531c734eeafb
Jie Zeng <jiezeng.zj@gmail.com>
2015-06-21 15:18:29 +0800

    update reward view&logic. user can have options for using the rewards.

--- js/ctrl-rewards.js
+++ js/ctrl-rewards.js
@@ -22,12 +22,48 @@ azrjCtrlRewards.controller('UserRewardsCtrl', ['$scope', '$routeParams', '$http'
     });
     
     $scope.pageName = '积分中心';
-    $scope.current_act = '';
-  
+    $scope.orderPro = '';
+
+    $scope.selOpts = [
+      {
+        name: '---请选择---'   // XXX: should be the first item for options.
+      },
+      {
+        name:'兑换', value:'exchange', opts: [
+                {name: '兑换列表1', value: '3'},
+                {name: '兑换列表2', value: '4'}
+            ]
+      },
+      {
+        name:'赠予', value:'grant', opts: [
+                {name: '赠予列表1', value: 'yyy'},
+                {name: '赠予列表2', value: 'yyyy'}
+            ]
+      },
+      {
+        name:'啥事不做', value:'nothing', opts: [
+                {name: '看看而已', value: 'zzz'},
+                {name: '路过炫耀', value: 'zzzz'}
+            ]
+      }
+    ];
+    
+    $scope.orderPro = $scope.selOpts[0].name;
+ 
     // methods in this controller.
     $scope.resetAct = function () {
         $scope.current_act = '';
     };
     
+    $scope.showList = function(str) {
+
+        //if (str == 'exchange') {
+            for (var i = 0; i < $scope.selOpts.length; i++) {
+                if ($scope.selOpts[i].value == str)
+                    return $scope.selOpts[i].name == $scope.orderPro;
+            }
+        //}    
+        return false;
+    }
     
   }]);
